fb68bc0d5561cb9c78251e2eae9343ece68254a9
Korea-code <rlawlgy93@gmail.com>
2020-03-04 23:19:59 -0500

    update

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 
-const general = require("./Controllers/general");
-const product = require("./Controllers/product");
-const products = require("./Controllers/products");
+const generalController = require("./Controllers/general");
+const productController = require("./Controllers/product");
+const productsController = require("./Controllers/products");
 
 const app = express();
 
@@ -16,9 +16,9 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", general);
-app.use("/product", product);
-app.use("/products", products);
+app.use("/", generalController);
+app.use("/product", productController);
+app.use("/products", productsController);
 
 const PORT = process.env.PORT;
 
